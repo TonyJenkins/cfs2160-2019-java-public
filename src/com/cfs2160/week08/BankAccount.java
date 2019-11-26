69d3c4a37e3dbac8d5a4a787780462687021129e
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2019-11-26 13:08:26 +0000

    Add Week 08 BankAccount skeleton code

---
+++ src/com/cfs2160/week08/BankAccount.java
@@ -0,0 +1,25 @@
+package com.cfs2160.week08;
+
+public class BankAccount {
+
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+    private boolean hasOverdraft;
+
+    // Constructor Here.
+    // Code -> Generate -> Constructor
+
+    // Getters and Setters Here.
+    // Code -> Generate -> Getters and Setters
+
+    // Deposit Method Here
+
+    // Withdraw Method Here.
+
+    // AddInterest Method Here.
+
+    // toString Method Here.
+    // Code -> Generate -> ...
+
+}
